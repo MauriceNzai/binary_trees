1733dfa28004c1477c3601761d58383a51dd4fc7
MauriceNzai <maurice.nzai@gmail.com>
2022-12-16 13:54:26 +0000

    Update Task

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -38,14 +38,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	if (tree->left)
-		left_size += 1 + binary_tree_height(tree->left);
+	left_size += 1 + binary_tree_height(tree->left);
 
-	if (tree->right)
-		right_size += 1 + binary_tree_height(tree->right);
+	right_size += 1 + binary_tree_height(tree->right);
 
 	if (left_size > right_size)
 		return (left_size);
-	else
-		return (right_size);
+	return (right_size);
 }
