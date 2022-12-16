d5e74b2051b4a746a9006aeec37fdc835bff5ee6
MauriceNzai <maurice.nzai@gmail.com>
2022-12-16 13:43:26 +0000

    Update Task

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -21,3 +21,31 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	return (left_height - right_height);
 }
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ *
+ * @tree: the binary tree to measure
+ *
+ * Return: The height of the tree
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_size = 0;
+	size_t right_size = 0;
+
+	if (!tree)
+		return (0);
+
+	if (tree->left)
+		left_size += 1 + binary_tree_height(tree->left);
+
+	if (tree->right)
+		right_size += 1 + binary_tree_height(tree->right);
+
+	if (left_size > right_size)
+		return (left_size);
+	else
+		return (right_size);
+}
