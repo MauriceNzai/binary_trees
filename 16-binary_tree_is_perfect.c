567712020eee75e40b5e4f46c3d00d87d23ab33f
MauriceNzai <maurice.nzai@gmail.com>
2022-12-17 08:36:03 +0000

    Update Task

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -5,63 +5,31 @@
  *
  * @tree: pointer to the root node of the binary tree to check
  *
- * Return: 1 if tree is full, 0 if tree is not full
+ * Return: 1 if tree is perfect, 0 if tree is not perfect
  */
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	int height;
+	int size;
+	int perfect_num;
+
 	if (!tree)
 		return (0);
 
-	if ((binary_tree_is_full(tree) == 1) &&
-			(binary_tree_balance(tree) == 0))
+	if (!tree->left && tree->right)
+		return (0);
+	if (tree->left && !tree->right)
+		return(0);
+
+	height = binary_tree_height(tree);
+	size = binary_tree_size(tree);
+	perfect_num = powered(2, height) - 1;
+
+	if (size == perfect_num)
 		return (1);
-	
-	return (0);
-}
-
-/**
- * binary_tree_is_full - checks if a binary tree is full
- *
- * @tree: pointer to the root node of the binary tree to check
- *
- * Return: 1 if tree is full, 0 if tree is not full
- */
-
-int binary_tree_is_full(const binary_tree_t *tree)
-{
-	if (!tree)
+	else
 		return (0);
-
-	if (!tree->left && !tree->right)
-		return (1);
-
-	if (tree->left && tree->right)
-		return (binary_tree_is_full(tree->left) &&
-			binary_tree_is_full(tree->right));
-	return (0);
-}
-
-/**
- * binary_tree_balance - measures the balance factor of a binary tree
- *
- * @tree: pointer to root node of the binary tree to measure balance factor
- *
- * Return: the balance factor of the tree
- */
-
-int binary_tree_balance(const binary_tree_t *tree)
-{
-	size_t left_height = 0;
-	size_t right_height = 0;
-
-	if (!tree)
-		return (0);
-
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-
-	return (left_height - right_height);
 }
 
 /**
@@ -81,11 +49,50 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 
 	left_size += 1 + binary_tree_height(tree->left);
-
 	right_size += 1 + binary_tree_height(tree->right);
 
 	if (left_size > right_size)
 		return (left_size);
 	else
 		return (right_size);
+}
+
+/**
+ * binary_tree_size - measures the size of a binary tree
+ *
+ * @tree: pointer to the root of the binary tree to measure
+ *
+ * Return: size of the binary tree
+ */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t size = 1;
+
+	if (!tree)
+		return (0);
+
+	size += binary_tree_size(tree->left);
+	size += binary_tree_size(tree->right);
+
+	return (size);
+}
+
+/**
+ * powered - returns value of a to power b
+ *
+ * @a: number to be raised to a power
+ * @b: the power/exponent
+ *
+ * Return: a power b( a ** b)
+ */
+
+int powered(int a, int b)
+{
+	if (b < 0)
+		return (-1);
+	if (b == 0)
+		return (1);
+	else
+		return (a * powered(a, b - 1));
 }
