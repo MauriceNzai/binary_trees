4e2289f0108d64e09b0bb55180713ed17e0a97e7
MauriceNzai <maurice.nzai@gmail.com>
2022-12-19 10:06:48 +0000

    Add function to chech if a binary tree is a valid BST

---
+++ 110-binary_tree_is_bst.c
@@ -0,0 +1,43 @@
+#include <limits.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_bst - Checks if a banary tree is a valid binary search tree
+ *
+ * @tree: pointer to the root node of the binary tree to check
+ *
+ * Return: 1 if tree is a valid BST, 0 if tree is not BST
+ */
+
+int binary_tree_is_bst(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	return (is_tree_bst(tree, INT_MIN, INT_MAX));
+}
+
+/**
+ * is_tree_bst - checks if tree is a valid BST
+ *
+ * @tree: pointer to the root node of the binary tre to search
+ * @min: value of the least node visited
+ * @max: value of the greatest node visited
+ *
+ * Return: 1 if tree is BST, 0 if tree is not BST
+ */
+
+int is_tree_bst(const binary_tree_t *tree, int min, int max)
+{
+	if (tree)
+	{
+		if (tree->n < min || tree->n > max)
+			return (0);
+
+		return(is_tree_bst(tree->left, min, tree->n -1) &&
+
+			is_tree_bst(tree->right, tree->n + 1, max));
+	}
+	return (1);
+}
+
